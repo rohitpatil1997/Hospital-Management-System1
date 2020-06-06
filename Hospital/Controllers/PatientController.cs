5a93397970b5d55a39c81e1708ed6c0d02b944ae
rohitpatil1997 <rp888867@gmail.com>
2020-06-06 21:15:37 +0530

    Importing project

---
+++ Hospital/Controllers/PatientController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hospital.Controllers
+{
+    public class PatientController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View("PatientAdd");
+        }
+        public IActionResult Submit(string PatientName, string PatientProblem)
+        {
+            return Content("You had Submitted " + PatientName
+                + " he has a Problem of " + PatientProblem);
+
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
